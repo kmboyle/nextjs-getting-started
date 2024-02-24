43f740ce89721b5a9bedeceb63a3a83e4e3aa03f
Kevin Boyle <kevin.michael.boyle@gmail.com>
2024-02-24 09:24:03 -0500

    ch 16 - metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -5,7 +5,11 @@ import { lusitana } from '@/app/ui/fonts';
 import { fetchCardData, fetchLatestInvoices } from '../../lib/data';
 import { CardSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
+import { Metadata } from 'next';
  
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 export default async function Page() {
     // const revenue = await fetchRevenue();
     // const latestInvoices = await fetchLatestInvoices();
