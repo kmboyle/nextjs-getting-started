e49f5a91c87fd7989c80f2ebef308084bfe7b943
Kevin Boyle <kevin.michael.boyle@gmail.com>
2025-07-23 16:51:37 -0400

    update and build fixes

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -9,28 +9,32 @@ export const metadata: Metadata = {
     title: 'Edit Invoice',
 };
 
-export default async function Page({ params }: { params: { id: string } }) {
-    const id = params.id;
-    const [ invoice, customers ] = await Promise.all([
-        fetchInvoiceById(id),
-        fetchCustomers()
-    ]);
-    if (!invoice) {
-        notFound();
-    }
-    return (
-        <main>
-            <Breadcrumbs
-                breadcrumbs={[
-                    { label: 'Invoices', href: '/dashboard/invoices' },
-                    {
-                        label: 'Edit Invoice',
-                        href: `/dashboard/invoices/${id}/edit`,
-                        active: true,
-                    },
-                ]}
-                />
-                <Form invoice={invoice} customers={customers} />
-        </main>
-    )
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+  if (!invoice) {
+    notFound();
+  }
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
 }
--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -12,32 +12,32 @@ export const metadata: Metadata = {
     title: 'Invoices',
 };
 export default async function Page({
-    searchParams,
+  searchParams,
 }: {
-    searchParams?: {
-        query?: string;
-        page?: string;
-    };
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
 }) {
-    const query = searchParams?.query || '';
-    const currentPage = Number(searchParams?.page) || 1;
-    const totalPages = await fetchInvoicesPages(query);
-    return (
-        <div className="w-full">
-            <div className="flex w-full items-center justify-between">
-                <h1 className={`${lusitana.className} text-2x1`}>Invoices</h1>
-            </div>
-            <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-                <Search placeholder="Search invoices..."/>
-                <CreateInvoice />
-            </div>
-            <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
-                <Table query={query} currentPage={currentPage} />
-            </Suspense>
-            <div className="mt-5 flex w-full justify-center">
-                <Pagination totalPages={totalPages} />
-            </div>
-
-        </div>
-    );
+  const resolvedSearchParams = await searchParams;
+  const query = resolvedSearchParams?.query || '';
+  const currentPage = Number(resolvedSearchParams?.page) || 1;
+  const totalPages = await fetchInvoicesPages(query);
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2x1`}>Invoices</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search invoices..." />
+        <CreateInvoice />
+      </div>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
 }
