f512a00813a0984b1f5b9c6f86d7c8f59a6725fb
Kevin Boyle <kevin.michael.boyle@gmail.com>
2025-07-24 17:16:19 -0400

    deploy

--- auth.ts
+++ auth.ts
@@ -20,25 +20,30 @@ export const { auth, signIn, signOut } = NextAuth({
     ...authConfig,
     providers: [
         Credentials({
-            async authorize(credentials) {
-              const parsedCredentials = z
-                .object({
-                    email: z.string().email(),
-                    password: z.string().min(6) })
-                .safeParse(credentials);
-              
-              if (parsedCredentials.success) {
-                const { email, password } = parsedCredentials.data;
-                const user = await getUser(email);
-                if (!user) return null;
-                const passwordsMatch = await bcrypt.compare(password, user.password);
-                
-                if (passwordsMatch) return user;
-              }
+          async authorize(credentials) {
+            const parsedCredentials = z
+              .object({
+                email: z.string().email(),
+                password: z.string().min(6)
+              })
+              .safeParse(credentials);
 
-              console.log('Invalid credentials');
-              return null;
+            if (parsedCredentials.success) {
+              const { email, password } = parsedCredentials.data;
+              const user = await getUser(email);
+              if (!user) return null;
+              const passwordsMatch = await bcrypt.compare(password, user.password);
+
+              if (passwordsMatch) return user;
             }
+
+            console.log('Invalid credentials');
+            return null;
+          },
+          credentials: {
+            username: { label: "Username", type: "text", placeholder: "jsmith" },
+            password: { label: "Password", type: "password" }
+          },
         })
     ],
 })
